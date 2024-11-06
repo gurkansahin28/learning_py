73fdad8fb41890131626f586ed50ee711ae7d395
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-11-06 15:19:01 +0300

    Update exception

---
+++ exception/built_in.py
@@ -0,0 +1,10 @@
+# Raising a ValueError with a custom message
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero!")
+    return a / b
+
+try:
+    result = divide(10, 0)
+except ValueError as e:
+    print(f"An error occurred: {e}")
