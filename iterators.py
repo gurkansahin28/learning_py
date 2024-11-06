7bf273389599a5c681ff3df193efa7128cc2a886
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-11-06 08:08:28 +0300

    Update

---
+++ iterators.py
@@ -0,0 +1,49 @@
+# iterators
+#---------------------------------------------------------------
+# the definition of the string of greeting
+greeting = 'hello'
+
+# printing letters of greeting
+for item in greeting:
+    print(item)
+    pass
+
+#---------------------------------------------------------------  
+print()      
+# the definition of the fruits list
+fruits = ['apple', 'banana', 'apricot']
+
+# printing list items and its letters through nested loops
+for item in fruits:
+    print(item)
+    for item in item:
+        print(item)
+    pass
+pass
+
+#---------------------------------------------------------------
+print()
+# defining a tupple
+vector = (3, 5, 7)
+
+for item in vector:
+    print(item)
+    pass
+    
+#---------------------------------------------------------------
+print()
+# defining a dictionary
+dictionary = {'name':'gurkan', 'PI':3.14, 'isSingle':False }
+    
+for key in dictionary:
+    print(key)
+    print(dictionary[key])
+    pass
+
+#---------------------------------------------------------------
+
+#---------------------------------------------------------------
+
+#---------------------------------------------------------------
+
+#---------------------------------------------------------------
